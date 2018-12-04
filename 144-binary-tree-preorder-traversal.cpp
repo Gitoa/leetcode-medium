01d8cc25cb28844e13c1ef6a1ac2b661ad923cb2
TT <gitoo1993@gmail.com>
2018-12-04 09:30:39 +0800

    144

---
+++ 144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* cur = s.top();
+            s.pop();
+            if(cur!=NULL){
+                ans.push_back(cur->val);
+                s.push(cur->right);
+                s.push(cur->left);
+            }
+        }
+        return ans;
+    }
+};
